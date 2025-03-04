18abae7c8607b029ab3d5cd0d227269272f5e2b2
Clemmie <379644692@qq.com>
2025-03-04 15:12:01 +0800

    Initial commit

---
+++ Example/SectorMenu/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SectorMenu
+//
+//  Created by Clemmie on 03/04/2025.
+//  Copyright (c) 2025 Clemmie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
